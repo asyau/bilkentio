e750a7b1f37e8787d324ebc2b2b5522e8d7440b4
Eren Berk Eraslan <eraslanerenberk@gmail.com>
2024-10-07 01:17:28 +0300

    backend initiated

---
+++ bilkentio-backend/src/main/java/com/example/bilkentio_backend/BilkentioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilkentio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilkentioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilkentioBackendApplication.class, args);
+	}
+
+}
---
+++ bilkentio-backend/src/test/java/com/example/bilkentio_backend/BilkentioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilkentio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilkentioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
