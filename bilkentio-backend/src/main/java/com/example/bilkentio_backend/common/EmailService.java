4d8265fce2c77d52216949d7de902b5bf82266e5
Baris Yayci <barsyayc@gmail.com>
2024-12-07 23:06:21 +0300

    added and tested email service

---
+++ bilkentio-backend/src/main/java/com/example/bilkentio_backend/common/EmailService.java
@@ -0,0 +1,26 @@
+package com.example.bilkentio_backend.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.sender.email}")
+    private String senderEmail;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(senderEmail);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
